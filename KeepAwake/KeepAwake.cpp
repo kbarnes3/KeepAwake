3f8e71fc46697809e1459bbbcd85140180233d1e
Kevin Barnes <kbarnes3@gmail.com>
2018-01-10 18:12:18 -0800

    The program blocks sleep

--- KeepAwake/KeepAwake.cpp
+++ KeepAwake/KeepAwake.cpp
@@ -4,8 +4,30 @@
 #include "stdafx.h"
 
 
-int main()
+void BlockSleep()
 {
+    EXECUTION_STATE result = NULL;
+
+    result = SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED);
+
+    if (result == NULL)
+    {
+        RaiseFailFastException(nullptr, nullptr, 0);
+    }
+}
+
+void DontExit()
+{
+
+}
+
+
+int wmain()
+{
+    BlockSleep();
+    wprintf(L"While this program is running, this computer won't sleep due to inactivity\n");
+    wprintf(L"Press Ctrl+C to end\n");
+    DontExit();
     return 0;
 }
 
